d3c9f70ef6fd607a35f76d8634ed26a698a0a6a1
Katarina Lukienko <ekaterina@tyrellcorp.work>
2020-03-21 13:22:56 +0200

    user_updating

--- mern-exercise-tracker/backend/routes/users.js
+++ mern-exercise-tracker/backend/routes/users.js
@@ -10,10 +10,35 @@ router.route('/').get((req, res) => {
 router.route('/add').post((req, res) => {
   const username = req.body.username;
 
-  const newUser = new User({username});
+  const newUser = new User({ username });
 
   newUser.save()
     .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req,res)=>{
+  User.findByIdAndDelete(req.params.id)
+  .then(()=>res.json('User deleted'))
+  .catch(err=> res.status(400).json('Error: '+ err));
+});
+
+
+router.route('/update/:id').post((req, res) => {
+  User.findById(req.body.id)
+    .then(user => {
+      user.username = req.body.username;
+
+      user.save()
+        .then(() => res.json('User updated'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
