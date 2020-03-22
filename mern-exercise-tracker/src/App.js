c92f4467248cc9384fbceb5289b92e2541111db5
Katarina Lukienko <ekaterina@tyrellcorp.work>
2020-03-22 10:04:41 +0200

    AddFront

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-class App extends Component {
-  render() {
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercises.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+
+
+
+function App() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="container">
+        <Navbar />
+        <br/>
+     <Route path='/' exact component={ExercisesList} />
+     <Route path='/edit/:id' exact component={EditExercise} />
+     <Route path='/create' exact component={CreateExercise} />
+     <Route path='/user' exact component={CreateUser} />
+     </div>
+      </Router>
     );
-  }
+  
 }
 
 export default App;
